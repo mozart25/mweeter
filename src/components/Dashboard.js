fabb879c1f725a627cff72789ca4aaf3dd4030c2
mozart <mozart@watchmile.com>
2021-01-30 23:06:54 +0900

    build dashboard basic setting

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,15 +1,33 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Dashboard extends Component {
 
     render() {
+        // console.log(this.props)
         return (
             <div>
-                Dashboard
+                <h3 className="center"> Your Timeline</h3>
+                <ul className="dashboard-list">
+                    {this.props.tweetIds.map((id) => (
+                        <li key={id}>
+                            <div>TWEET ID: {id}</div>
+                        </li>
+                    ))}
+
+                </ul>
             </div>
 
         )
     }
 }
 
-export default Dashboard
+function mapStateToProps({ tweets }) {
+    // console.log({ tweets })
+    return {
+        tweetIds: Object.keys(tweets)
+            .sort((a, b) => tweets[b].timestamp - tweets[a].timestemp)
+    }
+}
+
+export default connect(mapStateToProps)(Dashboard)
